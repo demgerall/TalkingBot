d703fe2193ec05720dee56f020edc8fc2e37ac82
demgerall <107266948+demgerall@users.noreply.github.com>
2023-01-17 00:03:01 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,246 @@
+import webbrowser
+
+from vosk import Model, KaldiRecognizer  # оффлайн-распознавание от Vosk
+import speech_recognition  # распознавание пользовательской речи (Speech-To-Text)
+import pyttsx3  # синтез речи (Text-To-Speech)
+import wave  # создание и чтение аудиофайлов формата wav
+import json  # работа с json-файлами и json-строками
+import os  # работа с файловой системой
+
+
+class VoiceAssistant:
+    """
+    Настройки голосового ассистента, включающие имя, пол, язык речи
+    """
+    name = ""
+    sex = ""
+    speech_language = ""
+    recognition_language = ""
+
+
+def setup_assistant_voice():
+    """
+    Установка голоса по умолчанию (индекс может меняться в
+    зависимости от настроек операционной системы)
+    """
+    voices = ttsEngine.getProperty("voices")
+
+    if assistant.speech_language == "en":
+        assistant.recognition_language = "en-US"
+        if assistant.sex == "female":
+            # Microsoft Zira Desktop - English (United States)
+            ttsEngine.setProperty("voice", voices[1].id)
+        else:
+            # Microsoft David Desktop - English (United States)
+            ttsEngine.setProperty("voice", voices[2].id)
+    else:
+        assistant.recognition_language = "ru-RU"
+        # Microsoft Irina Desktop - Russian
+        ttsEngine.setProperty("voice", voices[0].id)
+
+
+def play_voice_assistant_speech(text_to_speech):
+    """
+    Проигрывание речи ответов голосового ассистента (без сохранения аудио)
+    :param text_to_speech: текст, который нужно преобразовать в речь
+    """
+    ttsEngine.say(str(text_to_speech))
+    ttsEngine.runAndWait()
+
+
+def record_and_recognize_audio(*args: tuple):
+    """
+    Запись и распознавание аудио
+    """
+    with microphone:
+        recognized_data = ""
+
+        # регулирование уровня окружающего шума
+        recognizer.adjust_for_ambient_noise(microphone, duration=1)
+
+        try:
+            print("Слушаю...")
+            play_voice_assistant_speech("Слушаю...")
+            audio = recognizer.listen(microphone, 5, 5)
+
+            with open("microphone-results.wav", "wb") as file:
+                file.write(audio.get_wav_data())
+
+        except speech_recognition.WaitTimeoutError:
+            print("Can you check if your microphone is on, please?")
+            return
+
+        # использование online-распознавания через Google
+        # (высокое качество распознавания)
+        try:
+            print("Распознаю вашу речь...")
+            play_voice_assistant_speech("Распознаю вашу речь...")
+            recognized_data = recognizer.recognize_google(audio, language="ru").lower()
+
+        except speech_recognition.UnknownValueError:
+            pass
+
+        # в случае проблем с доступом в Интернет происходит
+        # попытка использовать offline-распознавание через Vosk
+        except speech_recognition.RequestError:
+            print("Trying to use offline recognition...")
+            recognized_data = use_offline_recognition()
+
+        return recognized_data
+
+
+def use_offline_recognition():
+    """
+    Переключение на оффлайн-распознавание речи
+    :return: распознанная фраза
+    """
+    recognized_data = ""
+    try:
+        # проверка наличия модели на нужном языке в каталоге приложения
+        if not os.path.exists("models/vosk-model-small-ru-0.4"):
+            print("Please download the model from:\n"
+                  "https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+            exit(1)
+
+        # анализ записанного в микрофон аудио (чтобы избежать повторов фразы)
+        wave_audio_file = wave.open("microphone-results.wav", "rb")
+        model = Model("models/vosk-model-small-ru-0.4")
+        offline_recognizer = KaldiRecognizer(model, wave_audio_file.getframerate())
+
+        data = wave_audio_file.readframes(wave_audio_file.getnframes())
+        if len(data) > 0:
+            if offline_recognizer.AcceptWaveform(data):
+                recognized_data = offline_recognizer.Result()
+
+                # получение данных распознанного текста из JSON-строки
+                # (чтобы можно было выдать по ней ответ)
+                recognized_data = json.loads(recognized_data)
+                recognized_data = recognized_data["text"]
+    except:
+        print("Sorry, speech service is unavailable. Try again later")
+
+    return recognized_data
+
+
+
+def execute_command_with_name(command_name: str, *args: list):
+    """
+    Выполнение заданной пользователем команды с дополнительными аргументами
+    :param command_name: название команды
+    :param args: аргументы, которые будут переданы в функцию
+    :return:
+    """
+    for key in commands.keys():
+        if command_name in key:
+            commands[key](*args)
+        else:
+            pass  # print("Command not found")
+
+def search_for_video_on_youtube(*args: tuple):
+    """
+    Поиск видео на YouTube с автоматическим открытием ссылки на список результатов
+    :param args: фраза поискового запроса
+    """
+    if not args[0]: return
+    search_term = " ".join(args[0])
+    url = "https://www.youtube.com/results?search_query=" + search_term
+    webbrowser.get().open(url)
+
+    # для мультиязычных голосовых ассистентов лучше создать
+    # отдельный класс, который будет брать перевод из JSON-файла
+    play_voice_assistant_speech("Вот какие видео я нашла по запросу " + search_term + "на ютубе")
+
+def search_in_internet(*args: tuple):
+    """
+    Поиск в интернете с автоматическим открытием ссылки на список результатов
+    :param args: фраза поискового запроса
+    """
+    if not args[0]: return
+    search_term = " ".join(args[0])
+    url = "https://yandex.ru/search/?clid=2358536&text=" + search_term
+    webbrowser.get().open(url)
+
+    # для мультиязычных голосовых ассистентов лучше создать
+    # отдельный класс, который будет брать перевод из JSON-файла
+    play_voice_assistant_speech("Вот что я нашла по запросу " + search_term + "в интернете")
+
+def play_greetings(*args: tuple):
+    """
+    Приветствие
+    """
+    play_voice_assistant_speech("Привет. Меня зовут Алиса")
+
+def play_name(*args: tuple):
+    """
+    Знакомство
+    """
+
+    play_voice_assistant_speech("Как вас зовут?")
+    voice_input = record_and_recognize_audio()
+    play_voice_assistant_speech("Приятно познакомиться, " + voice_input.split(" ")[0])
+
+def play_dela(*args: tuple):
+    """
+    Разговор
+    """
+
+    play_voice_assistant_speech("У меня все замечательно! Как у вас дела?")
+    voice_input = record_and_recognize_audio()
+    if "плохо" in voice_input:
+        play_voice_assistant_speech("Что случилось?")
+    else:
+        play_voice_assistant_speech("Рада слышать!")
+
+def play_farewell_and_quit(*args: tuple):
+    """
+    Прощание
+    """
+    play_voice_assistant_speech("Досвидания")
+    exit()
+
+
+commands = {
+    ("здравствуй", "привет"): play_greetings,
+    ("как дела"): play_dela,
+    ("найди"): search_in_internet,
+    ("как тебя зовут"): play_name,
+    ("выход", "стоп", "досвидания", "пока"): play_farewell_and_quit,
+    ("открой видео"): search_for_video_on_youtube,
+}
+
+if __name__ == "__main__":
+
+    # инициализация инструментов распознавания и ввода речи
+    recognizer = speech_recognition.Recognizer()
+    microphone = speech_recognition.Microphone()
+
+    # инициализация инструмента синтеза речи
+    ttsEngine = pyttsx3.init()
+
+    # настройка данных голосового помощника
+    assistant = VoiceAssistant()
+    assistant.name = "Алиса"
+    assistant.sex = "female"
+    assistant.speech_language = "ru"
+
+    # установка голоса по умолчанию
+    setup_assistant_voice()
+
+    while True:
+        # старт записи речи с последующим выводом распознанной речи
+        # и удалением записанного в микрофон аудио
+        voice_input = record_and_recognize_audio()
+        os.remove("microphone-results.wav")
+        print(voice_input)
+
+        # отделение команд от дополнительной информации (аргументов)
+        if "алиса" in voice_input:
+            voice_input = voice_input.replace('алиса ', '')
+            print(voice_input)
+            voice_input = voice_input.split(" ")
+            command = voice_input[0]
+            command_options = [str(input_part) for input_part in voice_input[1:len(voice_input)]]
+            execute_command_with_name(command, command_options)
+
+
+
